190839b2036e5b3cd4998caeb4fdaae6ca6e2e53
Troy Dai <troy.dai@outlook.com>
2018-01-04 16:35:35 -0800

    clean up yaml files and add namespaces

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+registry=azureclidev
+server=`az acr show -n $registry --query 'loginServer' -otsv`
 version=$(cat ./version)
-docker build -t a01store:$version .
+image=$server/a01store:$version
+
+
+az acr login -n $registry
+docker build -t $image .
+docker push $image
+
