97e11760f75481440f56027906c9eeef6f29a21f
Troy Dai <troy.dai@outlook.com>
2018-01-22 09:23:45 -0800

    Update: database schema and add script

--- Readme.md
+++ Readme.md
@@ -5,7 +5,7 @@ A01Store service accept automation tests scheduled by the Control (or user) and 
 
 # Basic
 - Python 3 (3.6)
-- Contaienr based
+- Container based
 - Flask
 
 # Deployments note
@@ -13,3 +13,16 @@ A01Store service accept automation tests scheduled by the Control (or user) and 
 - Set up context.
 - Set up secret. The vault is a0secret
 - Set up private docker registry secret using sp azureclidev-contributor
+
+# Database Migration Guid
+
+When the data model is changed the database scheme needs to be upgraded as well. This application relies 
+on the Flask-Migrate package to handle the data migrations.
+
+## Steps
+
+- Execute `. script/set_connection_str.sh` to set connection string. You need to have the access to
+  the a01store key vault.
+- Execute `export FLASK_APP=app/main`
+- Execute `flask db migrate`. Validate the migration
+- Execute `flask db upgrade`.
--- app/main.py
+++ app/main.py
@@ -43,17 +43,21 @@ def _unify_json_output(data):
 class Run(db.Model):
     # unique id
     id = db.Column(db.Integer, primary_key=True)
+
     # display name for the test run
     name = db.Column(db.String)
+
     # The test run settings is a immutable value. It is expected to be a JSON, however, it can be in any other form as
     # long as it can be represented in a string. The settings must not contain any secrets such as password or database
     # connection string. Those value should be sent to the test droid through Kubernete secret. And the values in the
     # settings can help the test droid locating to the correct secret value.
     settings = db.Column(db.String)
+
     # The details of the test run is mutable. It is expected to be a value bag allows the test system to store
     # information for analysis and presentation. The exact meaning and form of the value is decided by the application.
     # By default it is treated as a JSON object.
     details = db.Column(db.String)
+
     # The creation time of the run
     creation = db.Column(db.DateTime)
 
@@ -84,20 +88,20 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # display name for the test task
     name = db.Column(db.String)
-    # settings of the task. the settings can be saved in JSON or any other format defined by the application
-    settings = db.Column(db.String)
     # annotation of the task, used to fast query tasks under a run. its form is defined by the application.
     annotation = db.Column(db.String)
+    # settings of the task. the settings can be saved in JSON or any other format defined by the application. settings
+    # are immutable
+    settings = db.Column(db.String)
     # status of the task: initialized, scheduled, completed, and ignored
     status = db.Column(db.String)
-    # the duration of the test run in milliseconds
-    duration = db.Column(db.Integer)
+    # details of the task result. the value can be saved in JSON or any other format defined by the application. the
+    # result details are mutable
+    result_details = db.Column(db.String)
     # result of the test: passed, failed, and error
     result = db.Column(db.String)
-    # logging data of the test run
-    log = db.Column(db.String)
-    # details of the task result. the value can be saved in JSON or any other format defined by the application
-    result_details = db.Column(db.String)
+    # the duration of the test run in milliseconds
+    duration = db.Column(db.Integer)
 
     # relationship
     run_id = db.Column(db.Integer, db.ForeignKey('run.id'), nullable=False)
