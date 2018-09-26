681f37e3a9f40501ba04e4d7892a91fef6f7d468
Troy Dai <troy.dai@outlook.com>
2018-09-26 08:58:09 -0700

    Move data store to VSTS

--- services/store/scripts/run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-root=`cd $(dirname $0); cd ..; pwd`
-
-export A01_DATABASE_URI=`kubectl get secret store-secrets --template '{{ .data.dburi }}' | base64 -D`
-export FLASK_APP=$root/app/app/main.py
-export FLASK_DEBUG=1
-
-flask run
