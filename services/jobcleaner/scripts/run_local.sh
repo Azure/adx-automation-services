1ecb4f3bf280aeca5d04d631df68f50821532230
Troy Dai <troy.dai@outlook.com>
2018-09-26 10:32:53 -0700

    Move job cleaner to VSTS
    https://azure-cli.visualstudio.com/A01%20Automation%20Service/A01%20Automation%20Service%20Team/_git/A01%20Automation%20Job%20Cleaner

--- services/jobcleaner/scripts/run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run the job cleaner script locally for testing
-
-export A01_INTERNAL_COMKEY=`kubectl get secret store-secrets --template '{{ .data.comkey }}' | base64 -D`
-export A01_STORE_NAME='https://secondapi.azclitest.com'
-
-root=`cd $(dirname $0); cd ..; pwd`
-python $root/app/jobcleaner.py
